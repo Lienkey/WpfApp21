165329adbf9390f61000fc0f8651c99fb5c49b1c
олег <@LIENKEY>
2024-03-25 23:26:29 +0300

    Добавьте файлы проекта.

---
+++ Helper/FindPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Model;
+
+namespace WpfApp2.Helper
+{
+    internal class FindPerson
+    {
+        int id;
+
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+
+        public bool PersonPredicate(Person obj)
+        {
+            return obj.Id == id;
+        }
+    }
+}
---
+++ Helper/FindRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Model;
+
+namespace WpfApp2.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+
+}
